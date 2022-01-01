b3fb0a20ed038bb421c994d8d0c18aa5aa8f6084
jacknie <jacknie8407@gmail.com>
2022-01-01 21:53:34 +0900

    Init backend project

---
+++ backend-spring/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    application
+    idea
+    id("org.springframework.boot") version "2.6.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("kapt") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    kotlin("plugin.jpa") version "1.6.10"
+    kotlin("plugin.allopen") version "1.6.10"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    kapt("com.querydsl:querydsl-apt::jpa")
+
+    implementation(platform(kotlin("bom")))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-integration")
+    implementation("org.hibernate.validator:hibernate-validator")
+    implementation("com.querydsl:querydsl-jpa")
+    implementation("io.r2dbc:r2dbc-h2")
+
+    runtimeOnly("com.h2database:h2")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + listOf("-Xjsr305=strict", "-Xemit-jvm-type-annotations")
+        jvmTarget = "11"
+    }
+}
