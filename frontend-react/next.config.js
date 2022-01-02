8f0ace06ae5212c2445af3264fc7aaa07343b4db
jacknie <jacknie8407@gmail.com>
2022-01-02 16:40:52 +0900

    Add api proxy configuration

--- frontend-react/next.config.js
+++ frontend-react/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/:path*",
+      },
+    ];
+  },
+};
